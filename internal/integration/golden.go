d1b4f3ef18da017b6f4beace213b60e5e628ccf6
Vasiliy Vasilyuk <xorcare@gmail.com>
2019-10-16 18:00:06 +0300

    test: add golden integration tests
    Package integration is needed to test the functionality of golden
    in integration with the file system. Without side effects from
    other tests and TestMain.
    
    Large JSON inputs serve the purpose of format validation to provide an
    additional ability to catch changes in the behavior of the JSONEq
    function.

---
+++ internal/integration/golden.go
@@ -0,0 +1,10 @@
+// Copyright © 2019, Vasiliy Vasilyuk. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package integration is needed to test the functionality of golden
+in integration with the file system. Without side effects from
+other tests and TestMain.
+*/
+package integration
