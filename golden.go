4dc49401549ca2d0f76c6a436a16340e60c7d429
Vasiliy Vasilyuk <xorcare@gmail.com>
2024-01-28 22:42:52 +0300

    docs: update copyright
    Wrong year last time, oh those late night commits.

--- golden.go
+++ golden.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2023 Vasiliy Vasilyuk. All rights reserved.
+// Copyright (c) 2019-2024 Vasiliy Vasilyuk. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
